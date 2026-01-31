7a6fa530cfb6b114e18d451f6d977145b5bb62e0
baishnabikumari <baishnabi.5.06.2011@gmail.com>
2026-02-01 00:03:19 +0530

    added door in that big house with collisionlayers and added the layers so player can pass though it and also replaced the collision shape of the player to capsule as.

--- scripts/state_machine/node_state_machine.gd
+++ scripts/state_machine/node_state_machine.gd
@@ -25,7 +25,7 @@ func _physics_process(delta: float) -> void:
 	if current_node_state:
 		current_node_state._on_physics_process(delta)
 		current_node_state._on_next_transition()
-		print("Current State: ", current_node_state_name)
+		#print("Current State: ", current_node_state_name)
 		
 func transition_to(node_state_name : String) -> void:
 	if node_state_name == current_node_state.name.to_lower():
@@ -43,4 +43,4 @@ func transition_to(node_state_name : String) -> void:
 	
 	current_node_state = new_node_state
 	current_node_state_name = current_node_state.name.to_lower()
-	print("Current State: ", current_node_state_name)
+	#print("Current State: ", current_node_state_name)
