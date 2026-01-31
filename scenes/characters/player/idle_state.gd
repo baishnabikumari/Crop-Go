887e66aac65d80ffb468954b6ae4ff245097f675
baishnabikumari <baishnabi.5.06.2011@gmail.com>
2026-01-31 13:32:42 +0530

    added animations that is watering crops, tilling ground and chopping.

--- scenes/characters/player/idle_state.gd
+++ scenes/characters/player/idle_state.gd
@@ -23,6 +23,17 @@ func _on_next_transition() -> void:
 	
 	if GameInputEvents.is_movement_input():
 		transition.emit("walk")
+		
+	if player.current_tool == DataTypes.Tools.AxeWood && GameInputEvents.use_tool():
+		transition.emit("chopping")
+		
+	if player.current_tool == DataTypes.Tools.TillGround && GameInputEvents.use_tool():
+		transition.emit("tilling")
+		
+	if player.current_tool == DataTypes.Tools.WaterCrops && GameInputEvents.use_tool():
+		transition.emit("watering")
+		
+	
 
 func _on_enter() -> void:
 	pass
